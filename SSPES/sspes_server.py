d7df16af0d7f99add65ab33e8c055b435fc312de
Werner1403 <wmair@tsn.at>
2022-12-21 11:58:41 +0100

    added download

--- SSPES/sspes_server.py
+++ SSPES/sspes_server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file, send_from_directory
 from flask_restful import Api
 from sspes_rest import sspes_stats, player_stats
 
@@ -17,6 +17,11 @@ def success():
         os.remove('SSPES/sspes.json')
         f.save('SSPES/sspes.json')  
         return render_template("Acknowledgement.html", name = f.filename)
+    
+@app.route('/download')
+def download_file():
+	path = "sspes.json"
+	return send_file(path, as_attachment=True)
 
 api.add_resource(sspes_stats, '/stats')
 api.add_resource(player_stats, '/player/<string:p>')
